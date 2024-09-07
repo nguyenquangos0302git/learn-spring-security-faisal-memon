ad2cfe56944a1d17af5d55fa9669b4b8c3dee70f
nguyen quang <nguyenquangos0302git@gmail.com>
2024-09-07 07:35:35 +0700

    upload

---
+++ notes/src/main/java/com/secure/notes/services/UserService.java
@@ -0,0 +1,33 @@
+package com.secure.notes.services;
+
+import com.secure.notes.dtos.UserDTO;
+import com.secure.notes.models.Role;
+import com.secure.notes.models.User;
+
+import java.util.List;
+
+public interface UserService {
+    void updateUserRole(Long userId, String roleName);
+
+    List<User> getAllUsers();
+
+    UserDTO getUserById(Long id);
+
+    User findByUsername(String username);
+
+    void updateAccountLockStatus(Long userId, boolean lock);
+
+    List<Role> getAllRoles();
+
+    void updateAccountExpiryStatus(Long userId, boolean expire);
+
+    void updateAccountEnabledStatus(Long userId, boolean enabled);
+
+    void updateCredentialsExpiryStatus(Long userId, boolean expire);
+
+    void updatePassword(Long userId, String password);
+
+    void generatePasswordResetToken(String email);
+
+    void resetPassword(String token, String newPassword);
+}
